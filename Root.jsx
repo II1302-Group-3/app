654ade230e00613da7da6024279031ae953a61a4
Yasniaz <gigabo21@gmail.com>
2023-04-28 13:15:27 +0200

    iphone works

--- Root.jsx
+++ Root.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './store/store';
+import { LogBox } from 'react-native';
+LogBox.ignoreLogs(['new NativeEventEmitter']); // Ignore log notification by message
+LogBox.ignoreAllLogs(); //Ignore all log notifications
 
 function Root() {
-    return(
+    return (
         <Provider store={store}>
             <App />
         </Provider>
