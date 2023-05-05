4b5a4d79d5447e8dd5941b16b6df0a90d3f5a25e
Hannes Mann <hannesmann2000@gmail.com>
2023-05-05 09:19:20 +0200

    Merge code from KC

--- store/slices/templateValue.js
+++ store/slices/templateValue.js
@@ -1,5 +1,6 @@
 import { createSlice, createAsyncThunk} from "@reduxjs/toolkit";
 import database from '@react-native-firebase/database';
+import { Alert } from "react-native";
 
 const initialState = {
     tempLight: 50,
@@ -11,12 +12,12 @@ export const saveTemplate = createAsyncThunk(
     'templateValue/saveTemplate',
     async({tempLight, tempmoisture, tempName, uid}) => {
         const templateRef = 'templates'
-        const newChildRef = database().ref(templateRef).push(); 
-        newChildRef.set({plantName: tempName, lightLevel: tempLight, moistureLevel: tempmoisture }); 
+        const newChildRef = database().ref(templateRef).push();
+        newChildRef.set({plantName: tempName, lightLevel: tempLight, moistureLevel: tempmoisture });
         const templateKey = newChildRef.key;
         const userTemplateRef =  `users/${uid}/` + 'templates';
         database().ref(userTemplateRef).push().set({templateKey: templateKey});
-            
+        Alert.alert('Template saved successfully!');
     }
 )
 
