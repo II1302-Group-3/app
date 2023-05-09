ffd6269d85bf61a0d1d28cea5f94a7521ce8daf9
Yasniaz <gigabo21@gmail.com>
2023-05-09 16:25:07 +0200

    heart like

--- components/BrowseTemlates/BrowseTemplateNameView.jsx
+++ components/BrowseTemlates/BrowseTemplateNameView.jsx
@@ -2,27 +2,27 @@ import React, { useState } from 'react';
 import { View, Text, Image, TouchableOpacity } from 'react-native';
 import { TextInput } from 'react-native-paper';
 
-export const BrowseTemplateNameView = ({ plantName, tempDetailPress,  plantMoisture, plantLight, templatesData}) => {
+export const BrowseTemplateNameView = ({ plantName, tempDetailPress, plantMoisture, plantLight, templatesData }) => {
   const [searchText, setSearchText] = useState('');
 
   const filteredPlantNames = templatesData.filter(name =>
     name.plantName.toLowerCase().includes(searchText.toLowerCase())
   );
-  
+
   return (
     <View>
-        <View style={{margin:'5%',}}>
+      <View style={{ margin: '5%', }}>
         <TextInput
-        label="Search"
-        value={searchText}
-        onChangeText={text => setSearchText(text)}
-      />
-    </View>
- 
+          label="Search"
+          value={searchText}
+          onChangeText={text => setSearchText(text)}
+        />
+      </View>
+
       <View style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}>
         {filteredPlantNames.map((name, index) => (
           <TouchableOpacity
-              
+
             key={index}
             style={{
               backgroundColor: '#90ee90',
@@ -30,21 +30,25 @@ export const BrowseTemplateNameView = ({ plantName, tempDetailPress,  plantMoist
               padding: '3%',
               margin: '2%',
               elevation: 6,
-              width: '45%', 
+              width: '45%',
               height: 80,
             }}
             onPress={() => tempDetailPress(name.plantName, name.moistureLevel, name.lightLevel)}
           >
             <Text style={{ fontWeight: 'bold', fontSize: 25 }}>{name.plantName}</Text>
+            <Image
+              source={require('../../assets/heart.png')}
+              style={{ width: 20, height: 20, marginRight: 6 }}
+            ></Image>
             <View style={{ flexDirection: 'row' }}>
               <Image
                 source={require('../../assets/WaterDropIcon.png')}
                 style={{ width: 20, height: 20, marginRight: 6 }}
               />
 
-            <Text style={{ marginRight: 10 }}>{name.moistureLevel}</Text>
-        
-              
+              <Text style={{ marginRight: 10 }}>{name.moistureLevel}</Text>
+
+
               <Image
                 source={require('../../assets/BulpIcon.png')}
                 style={{ width: 20, height: 20, marginRight: 6 }}
