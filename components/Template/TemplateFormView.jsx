1a025b4178bbc18d345f2ffa91fe6a2dde6d6ffd
Hannes Mann <hannesmann2000@gmail.com>
2023-05-15 15:45:32 +0200

    Fix scrolling in add template

--- components/Template/TemplateFormView.jsx
+++ components/Template/TemplateFormView.jsx
@@ -5,6 +5,7 @@ import { EnvironmentSettingsSwitch } from '../Garden/EnvironmentSettings/Environ
 import { useHeaderHeight } from '@react-navigation/elements';
 import { Button, TextInput } from 'react-native-paper';
 import { template } from '@babel/core';
+import { ScrollView } from 'react-native-gesture-handler';
 
 
 const waterDropIcon = require('../../assets/WaterDropIcon.png'); // Link to waterDropIcon for the slider
@@ -27,51 +28,55 @@ export const TemplateFormView = ({
 
 	const headerHeight = useHeaderHeight();
 
+	// https://stackoverflow.com/questions/32664397/react-native-vertical-centering-when-using-scrollview
     return(
-        <View style={{paddingHorizontal: 20, paddingTop: 30, paddingBottom: 30 + headerHeight, justifyContent: "center", flexDirection: "column", height: "100%" }}>
-         	<View style={{ marginBottom: 48}}>
-				<TextInput
-					label="Name your plant"
-					mode="outlined"
-					value={name}
-					onChangeText={ value => setTemplateName(value)}
-				/>
-            </View>
+		<View style={{height: "100%", marginBottom: headerHeight}}>
+			<ScrollView style={{ paddingHorizontal: 20 }} contentContainerStyle={{ flexGrow: 1, justifyContent: "center" }}>
+				<View style={{paddingVertical: 30}}>
+					<View style={{ marginBottom: 48}}>
+						<TextInput
+							label="Name your plant"
+							mode="outlined"
+							value={name}
+							onChangeText={ value => setTemplateName(value)}
+						/>
+					</View>
 
-            <View style={{marginBottom: 20}}>
-				<EnvironmentSettingsSliderView
-					title={ moistureTitle }
-					unit={ "%" }
-					sliderIcon={ waterDropIcon }
-					onSlide={ setMoisture }
-					advancedInfo={ advancedInfo }
-					nutrition={moisture}
-					highRange={false}
-				/>
-			</View>
-			<EnvironmentSettingsSliderView
-				title={ lightTitle }
-				unit={ "lux" }
-				sliderIcon={ bulbIcon }
-				onSlide={ setLight }
-				advancedInfo={ advancedInfo }
-				nutrition={light}
-				highRange={true}
-			/>
+					<View style={{marginBottom: 20}}>
+						<EnvironmentSettingsSliderView
+							title={ moistureTitle }
+							unit={ "%" }
+							sliderIcon={ waterDropIcon }
+							onSlide={ setMoisture }
+							advancedInfo={ advancedInfo }
+							nutrition={moisture}
+							highRange={false}
+						/>
+					</View>
+					<EnvironmentSettingsSliderView
+						title={ lightTitle }
+						unit={ "lux" }
+						sliderIcon={ bulbIcon }
+						onSlide={ setLight }
+						advancedInfo={ advancedInfo }
+						nutrition={light}
+						highRange={true}
+					/>
 
-			<View style={{ paddingVertical: 20 }}>
-				<EnvironmentSettingsSwitch
-					title={ advancedInfoTitle }
-					onToggle={ setAdvancedInfo }
-					active={ advancedInfo }
-				/>
-			</View>
+					<View style={{ paddingVertical: 20 }}>
+						<EnvironmentSettingsSwitch
+							title={ advancedInfoTitle }
+							onToggle={ setAdvancedInfo }
+							active={ advancedInfo }
+						/>
+					</View>
 
-            <View>
-                <Button mode="contained" style={{marginTop: 30}} onPress={() => saveTemplateValue()} disabled={name.trim().length < 3}>Save template</Button>
-            </View>
+					<View>
+						<Button mode="contained" style={{marginTop: 30}} onPress={() => saveTemplateValue()} disabled={name.trim().length < 3}>Save template</Button>
+					</View>
+				</View>
 
+			</ScrollView>
 		</View>
-
     )
 }
